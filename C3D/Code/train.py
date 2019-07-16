347cce8f9ef891d2173488a3d2f9ee8f3a143b11
Xiaogang <lxiganglav@foxmail.com>
2019-07-16 15:45:46 +0800

    Update train.py

--- C3D/Code/train.py
+++ C3D/Code/train.py
@@ -15,7 +15,7 @@ import save_inference_model
 def load_clip_name(path,status):
     labels = pd.read_csv(os.path.join(path,'Label_Map','label.txt'))    # load label.txt
     all_clips_name = rd.read_dataset(path,labels,status,seed=66,balance=True)    # train set
-    mean_image = np.load(os.path.join(path,'Data',status,'mean_image.npy'))    # read mean image
+    mean_image = np.load(os.path.join(path,'Data','Train','mean_image.npy'))    # read mean image
     return all_clips_name,mean_image
         
  
